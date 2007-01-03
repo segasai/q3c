8b13eed0ca690eb6d645376d68c16d55268e3f0f
Sergey Koposov <math@8c088d1f-841a-aa82-adcb-f5b00ec8af86>
2007-01-03 23:18:49 +0000

    Indenting changes
    git-svn-id: https://q3c.googlecode.com/svn/trunk@75 8c088d1f-841a-aa82-adcb-f5b00ec8af86

--- q3c_poly.c
+++ q3c_poly.c
@@ -417,186 +417,181 @@ int q3c_check_sphere_point_in_poly(struct q3c_prm *hprm, int n,
                                    q3c_coord_t ra0, q3c_coord_t dec0,
                                    int invocation)
 {
-  q3c_coord_t xmin,xmax,ymin, ymax;
-  static char faces[6], multi_flag;
-  q3c_ipix_t ipix;
-  q3c_coord_t points[4];
-  char face_num, face_num0, cur_face_num;
-  static q3c_coord_t x[3][Q3C_MAX_N_POLY_VERTEX], y[3][Q3C_MAX_N_POLY_VERTEX],
-  	ax[3][Q3C_MAX_N_POLY_VERTEX], ay[3][Q3C_MAX_N_POLY_VERTEX], x0, y0;
-  int face_count = -1, i;
-  
-
-  q3c_poly qp;
-  
-  ang2ipix_xy(hprm, ra0, dec0, &cur_face_num, &ipix, &x0, &y0);
-
-  qp.ra = in_ra;
-  qp.dec = in_dec;  
-  qp.n = n;
-  
-  if (invocation == 0)
-  { 
-    face_num = q3c_get_facenum_poly(&qp);
-    faces[0] = face_num;
-
-    qp.x = x[0];
-    qp.y = y[0];
-    qp.ax = ax[0];
-    qp.ay = ay[0];
-    
-
-    q3c_project_poly(&qp, face_num);
-    q3c_prepare_poly(&qp);
-    
-    q3c_get_minmax_poly(&qp, &xmin, &xmax, &ymin, &ymax);
-    
-
-    /* Now in a little bit ugly but fastest way I determine whether the ellipse 
-     * intersect other faces or not, and if yes, I setup the array "points" to the
-     * multi_face loop.
-     */
-    if (xmin < -Q3C_HALF)
-    {
-      if (ymin < -Q3C_HALF)
-      {
-        points[0] = xmax;
-        points[1] = ymin;
-        points[2] = xmin;
-        points[3] = ymax;
-        multi_flag = 2;
-        xmin = -Q3C_HALF;
-        ymin = -Q3C_HALF;
-      }
-      else 
-      {
-        if (ymax > Q3C_HALF)
-        {
-          points[0] = xmax;
-          points[1] = ymax;
-          points[2] = xmin;
-          points[3] = ymin;
-          multi_flag = 2;
-          xmin = -Q3C_HALF;
-          ymax = Q3C_HALF;
-        }
-        else
-        {
-          points[0] = xmin;
-          points[1] = (ymin + ymax) / 2;
-          multi_flag = 1;      
-          xmin = -Q3C_HALF;
-        }
-      }
-    }
-    else 
-    {
-      if (xmax > Q3C_HALF)
-      {
-        if (ymin < -Q3C_HALF)
-        {
-          points[0] = xmin;
-          points[1] = ymin;
-          points[2] = xmax;
-          points[3] = ymax;
-          multi_flag = 2;      
-          xmax = Q3C_HALF;
-          ymin = -Q3C_HALF;
-        }
-        else
-        {
-          if (ymax > Q3C_HALF)
-          {
-            points[0] = xmin;
-            points[1] = ymax;
-            points[2] = xmax;
-            points[3] = ymax;
-            multi_flag = 2;
-            xmax = Q3C_HALF;
-            ymax = Q3C_HALF;
-          }
-          else
-          {
-            points[0] = xmax;
-            points[1] = (ymin + ymax) / 2;
-            multi_flag = 1;
-            xmax = Q3C_HALF;
-          }
-        }
-      }
-      else
-      {
-        if (ymin < -Q3C_HALF)
-        {
-          points[0] = (xmin + xmax) / 2;
-          points[1] = ymin;
-          multi_flag = 1;
-          ymin = -Q3C_HALF;
-        }
-        else 
-        {
-          if (ymax > Q3C_HALF)
-          {
-            points[0] = (xmin + xmax) / 2;
-            points[1] = ymax;
-            multi_flag = 2;
-            ymax = Q3C_HALF;
-          }
-          else
-          {
-            multi_flag = 0;
-          }
-        }
-      }
-    }
-
-    face_num0 = face_num;
-    
-    for(face_count = 0; face_count <= multi_flag; face_count++)
-    {
-      /* This the beginning of the mega-loop over multiple faces */
-      
-      
-      if (face_count > 0)
-        /* This "if" works when we pass through the secondary faces */ 
-      {
-        face_num = q3c_xy2facenum(2 * points[2 * face_count - 2],
-                      2 * points[2 * face_count - 1], face_num0);
-              
-        faces[face_count] = face_num;
-
-        qp.x = x[face_count];
-        qp.y = y[face_count];
-        qp.ax = ax[face_count];
-        qp.ay = ay[face_count];
-        
-        q3c_project_poly(&qp, faces[face_count]);
-
-        q3c_prepare_poly(&qp);
-      }      
-    }
-  }
-
-
-  for (i = 0; i <= multi_flag; i++)
-  {
-    if (faces[i] == cur_face_num)
-    {
-      face_count = i;
-      break;
-    }
-  }
-
-  if (i == (multi_flag + 1))
-  {
-    return 0;
-  }
-
-  qp.x = x[face_count];
-  qp.y = y[face_count];
-  qp.ax = ax[face_count];
-  qp.ay = ay[face_count];
-
-
-  return q3c_check_point_in_poly(&qp, x0, y0);
+	q3c_coord_t xmin,xmax,ymin, ymax;
+	static char faces[6], multi_flag;
+	q3c_ipix_t ipix;
+	q3c_coord_t points[4];
+	char face_num, face_num0, cur_face_num;
+	static q3c_coord_t x[3][Q3C_MAX_N_POLY_VERTEX], y[3][Q3C_MAX_N_POLY_VERTEX],
+		ax[3][Q3C_MAX_N_POLY_VERTEX], ay[3][Q3C_MAX_N_POLY_VERTEX], x0, y0;
+	
+	int face_count = -1, i;
+	
+	q3c_poly qp;
+	
+	ang2ipix_xy(hprm, ra0, dec0, &cur_face_num, &ipix, &x0, &y0);
+	
+	qp.ra = in_ra;
+	qp.dec = in_dec;
+	qp.n = n;
+	
+	if (invocation == 0)
+	{
+		face_num = q3c_get_facenum_poly(&qp);
+		faces[0] = face_num;
+		
+		qp.x = x[0];
+		qp.y = y[0];
+		qp.ax = ax[0];
+		qp.ay = ay[0];
+		
+		q3c_project_poly(&qp, face_num);
+		q3c_prepare_poly(&qp);
+		
+		q3c_get_minmax_poly(&qp, &xmin, &xmax, &ymin, &ymax);
+		
+		/* Now in a little bit ugly but fastest way I determine whether the ellipse
+		 * intersect other faces or not, and if yes, I setup the array "points" to the
+		 * multi_face loop.
+		 */
+		
+		if (xmin < -Q3C_HALF)
+		{
+			if (ymin < -Q3C_HALF)
+			{
+				points[0] = xmax;
+				points[1] = ymin;
+				points[2] = xmin;
+				points[3] = ymax;
+				multi_flag = 2;
+				xmin = -Q3C_HALF;
+				ymin = -Q3C_HALF;
+			}
+			else
+			{
+				if (ymax > Q3C_HALF)
+				{
+					points[0] = xmax;
+					points[1] = ymax;
+					points[2] = xmin;
+					points[3] = ymin;
+					multi_flag = 2;
+					xmin = -Q3C_HALF;
+					ymax = Q3C_HALF;
+				}
+				else
+				{
+					points[0] = xmin;
+					points[1] = (ymin + ymax) / 2;
+					multi_flag = 1;
+					xmin = -Q3C_HALF;
+				}
+			}
+		}
+		else 
+		{
+			if (xmax > Q3C_HALF)
+			{
+				if (ymin < -Q3C_HALF)
+				{
+					points[0] = xmin;
+					points[1] = ymin;
+					points[2] = xmax;
+					points[3] = ymax;
+					multi_flag = 2;
+					xmax = Q3C_HALF;
+					ymin = -Q3C_HALF;
+				}
+				else
+				{
+					if (ymax > Q3C_HALF)
+					{
+						points[0] = xmin;
+						points[1] = ymax;
+						points[2] = xmax;
+						points[3] = ymax;
+						multi_flag = 2;
+						xmax = Q3C_HALF;
+						ymax = Q3C_HALF;
+					}
+					else
+					{
+						points[0] = xmax;
+						points[1] = (ymin + ymax) / 2;
+						multi_flag = 1;
+						xmax = Q3C_HALF;
+					}
+				}
+			}
+			else
+			{
+				if (ymin < -Q3C_HALF)
+				{
+					points[0] = (xmin + xmax) / 2;
+					points[1] = ymin;
+					multi_flag = 1;
+					ymin = -Q3C_HALF;
+				}
+				else
+				{
+					if (ymax > Q3C_HALF)
+					{
+						points[0] = (xmin + xmax) / 2;
+						points[1] = ymax;
+						multi_flag = 2;
+						ymax = Q3C_HALF;
+					}
+					else
+					{
+						multi_flag = 0;
+					}
+				}
+			}
+		}
+		
+		face_num0 = face_num;
+		
+		for(face_count = 0; face_count <= multi_flag; face_count++)
+		{
+		/* This the beginning of the mega-loop over multiple faces */
+			
+			if (face_count > 0)
+			/* This "if" works when we pass through the secondary faces */
+			{
+				face_num = q3c_xy2facenum(2 * points[2 * face_count - 2],
+					2 * points[2 * face_count - 1], face_num0);
+				
+				faces[face_count] = face_num;
+				
+				qp.x = x[face_count];
+				qp.y = y[face_count];
+				qp.ax = ax[face_count];
+				qp.ay = ay[face_count];
+				
+				q3c_project_poly(&qp, faces[face_count]);
+				q3c_prepare_poly(&qp);
+			}
+		}
+	}
+	
+	for (i = 0; i <= multi_flag; i++)
+	{
+		if (faces[i] == cur_face_num)
+		{
+			face_count = i;
+			break;
+		}
+	}
+	
+	if (i == (multi_flag + 1))
+	{
+		return 0;
+	}
+	
+	qp.x = x[face_count];
+	qp.y = y[face_count];
+	qp.ax = ax[face_count];
+	qp.ay = ay[face_count];
+	
+	return q3c_check_point_in_poly(&qp, x0, y0);
 }
