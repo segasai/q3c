9e302e3933261eb599662b906dfa97ab4fedd874
Sergey Koposov <skoposov@cmu.edu>
2018-02-01 15:01:17 -0500

    add forgotten sindist function

--- scripts/q3c--1.5.0.sql
+++ scripts/q3c--1.5.0.sql
@@ -59,6 +59,16 @@ CREATE OR REPLACE FUNCTION q3c_sindist(double precision, double precision,
         AS 'MODULE_PATHNAME', 'pgq3c_sindist'
         LANGUAGE C IMMUTABLE STRICT COST 100;
 
+CREATE OR REPLACE FUNCTION q3c_sindist_pm(
+       ra1 double precision, dec1 double precision,
+       pmra1 double precision, pmdec1 double precision,
+       epoch1 double precision, ra2 double precision, dec2 double precision,
+       epoch2 double precision)
+        RETURNS double precision
+        AS 'MODULE_PATHNAME', 'pgq3c_sindist_pm'
+        LANGUAGE C IMMUTABLE COST 100;
+-- Importantly this is not strict 
+
 CREATE OR REPLACE FUNCTION q3c_nearby_it(double precision, double precision, 
 					 double precision, integer)
         RETURNS bigint
