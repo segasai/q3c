50ec4155b5ddb96b5f257719b5d92c95c615eee9
Sergey Koposov <skoposov@cmu.edu>
2020-02-04 23:31:11 -0500

    update the test

--- sql/join_pm2.sql
+++ sql/join_pm2.sql
@@ -1,21 +1,21 @@
 select count(*) from test_pm1 as a, test as b where q3c_join (
-       a.ra+a.pmra/3600/1000*(2000-epoch),
+       a.ra+a.pmra/3600/1000/cos(radians(a.dec))*(2000-epoch),
        a.dec+a.pmdec/3600/1000*(2000-epoch),
        b.ra,b.dec,0.002);
 select count(*) from test_pm1 as a, test as b where q3c_join(
-       a.ra+a.pmra/3600/1000*(2000-epoch),
+       a.ra+a.pmra/3600/1000/cos(radians(a.dec))*(2000-epoch),
        a.dec+a.pmdec/3600/1000*(2000-epoch),
        b.ra,b.dec,0.004);
 select count(*) from test_pm1 as a, test as b where q3c_join(
-       a.ra+a.pmra/3600/1000*(2000-epoch),
+       a.ra+a.pmra/3600/1000/cos(radians(a.dec))*(2000-epoch),
        a.dec+a.pmdec/3600/1000*(2000-epoch),
        b.ra,b.dec,0.01);
 select count(*) from test_pm1 as a, test as b where q3c_join(
-       a.ra+a.pmra/3600/1000*(2000-epoch),
+       a.ra+a.pmra/3600/1000/cos(radians(a.dec))*(2000-epoch),
        a.dec+a.pmdec/3600/1000*(2000-epoch),
        b.ra,b.dec,0.02);
 select count(*) from test_pm1 as a, test as b where q3c_join(
-       a.ra+a.pmra/3600/1000*(2000-epoch),
+       a.ra+a.pmra/3600/1000/cos(radians(a.dec))*(2000-epoch),
        a.dec+a.pmdec/3600/1000*(2000-epoch),
        b.ra,b.dec,0.042);
 
