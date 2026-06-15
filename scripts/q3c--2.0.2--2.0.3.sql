5028bcc538fac08acb9ad8e669fcb3cea5cd630b
Sergey E. Koposov <skoposov@ed.ac.uk>
2026-06-15 13:20:32 +0100

    add scripts

---
+++ scripts/q3c--2.0.2--2.0.3.sql
@@ -0,0 +1,4 @@
+\echo Use "ALTER EXTENSION q3c UPDATE TO '2.0.3'" to load this file. \quit
+
+-- No SQL objects changed in 2.0.3. The release updates the C implementation
+-- to validate internal iteration arguments before indexing cached arrays.
